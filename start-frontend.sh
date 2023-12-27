2518e0dfc6fd156170afe896259f260284904be1
flandrecorentin <flandre@kth.se>
2023-12-27 18:45:03 +0100

    Update bash file

---
+++ start-frontend.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# pull last release on master
+sudo git checkout master
+sudo git pull
+
+# build frontend into dist/
+cd ./frontendstravaintegration
+sudo ng build
+
+# copy dist/ into nginx 
+sudo cp -r /dist/frontend-stravaintegration/* /var/www/stravaintegration/
+
+# restart nginx
+sudo service nginx restart
